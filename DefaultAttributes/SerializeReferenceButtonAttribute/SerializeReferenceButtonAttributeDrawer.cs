1a0b6a590c80a9089499dce26f334316210ed78c
kaiyumPC <kaiyumcg@gmail.com>
2022-11-24 21:15:35 +0600

    init commit with fixes

--- DefaultAttributes/SerializeReferenceButtonAttribute/SerializeReferenceButtonAttributeDrawer.cs
+++ DefaultAttributes/SerializeReferenceButtonAttribute/SerializeReferenceButtonAttributeDrawer.cs
@@ -13,19 +13,37 @@ public class SerializeReferenceButtonAttributeDrawer : PropertyDrawer
  
     public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
     {
+        try
+        {
+            EditorGUI.BeginProperty(position, label, property);
 
-        
-        EditorGUI.BeginProperty(position, label, property); 
-        
-        var labelPosition = new Rect(position.x, position.y, position.width, EditorGUIUtility.singleLineHeight);
-        EditorGUI.LabelField(labelPosition, label);    
-         
-        var typeRestrictions = SerializedReferenceUIDefaultTypeRestrictions.GetAllBuiltInTypeRestrictions(fieldInfo);
-        property.DrawSelectionButtonForManagedReference(position, typeRestrictions);
-        
-        EditorGUI.PropertyField(position, property, GUIContent.none, true);
-        
-        EditorGUI.EndProperty(); 
+            var labelPosition = new Rect(position.x, position.y, position.width, EditorGUIUtility.singleLineHeight);
+            EditorGUI.LabelField(labelPosition, label);
+
+            var typeRestrictions = SerializedReferenceUIDefaultTypeRestrictions.GetAllBuiltInTypeRestrictions(fieldInfo);
+            property.DrawSelectionButtonForManagedReference(position, typeRestrictions);
+
+            if (property.isArray)
+            {
+                if (property.arraySize < 0) { property.arraySize = 0; }
+            }
+            try
+            {
+                EditorGUI.PropertyField(position, property, GUIContent.none, true);
+            }
+            catch (System.Exception ex)
+            {
+                Debug.Log("ex 1: " + ex.Message + " display name: " + property.displayName + " and name: " + property.name+" is array type: "+property.isArray);
+            }
+            
+
+            EditorGUI.EndProperty();
+        }
+        catch (System.Exception ex)
+        {
+            Debug.Log("ex 2: " + ex.Message + " display name: " + property.displayName + " and name: " + property.name + " is array type: " + property.isArray);
+        }
+
     } 
 }
 #endif
